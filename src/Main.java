95a858c2fb170490f665f92f41f02f750e5ccbeb
soyLucero <queen.luceroaccostupa@gmail.com>
2024-05-08 10:35:07 -0500

    version base

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    static int iEdad  = 20;
+
+    public static void main(String[] args) {
+        //Punto de Partida: método main
+        saludar(50);
+        int iEdad  = 30;
+        System.out.println(iEdad);
+        saludar(78);
+    }
+
+    public static void saludar(int iEdad) {
+        System.out.println("Hola: " + iEdad);
+        System.out.println("Haciendo cambios");
+    }
+}
